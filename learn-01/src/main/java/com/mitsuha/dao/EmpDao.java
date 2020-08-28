c1045f10da512c779a95c6e0d76c4216fdd9c701
MitsuhaCon <mitsuhaCon@gmail.com>
2020-08-29 00:58:06 +0800

    使用注解的方式执行 sql

--- learn-01/src/main/java/com/mitsuha/dao/EmpDao.java
+++ learn-01/src/main/java/com/mitsuha/dao/EmpDao.java
@@ -1,6 +1,7 @@
 package com.mitsuha.dao;
 
 import com.mitsuha.pojo.Emp;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -11,5 +12,19 @@ import java.util.List;
  * @data 2020/8/27 0:30
  */
 public interface EmpDao {
+    /**
+     * 获取 emp 表中的所有数据
+     * @return
+     */
     List<Emp> getEmpList();
+
+    /**
+     * 通过 enpno 查找用户信息
+     * @param empno
+     * @return
+     */
+    Emp getEmpByEmpno(int empno);
+
+    @Select("select * from emp where empno = #{empno}")
+    Emp getEmpByAnnotation(int empno);
 }
